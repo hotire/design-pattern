d67d9d6accdbd261ad532b9880622af6be9cbd4d
hotire <gngh0101@gmail.com>
2019-08-30 21:15:19 +0900

    Fix static string so that

--- designPattern/src/main/singleton/SingletonHolder.java
+++ designPattern/src/main/singleton/SingletonHolder.java
@@ -8,9 +8,9 @@ package singleton;
 public class SingletonHolder {
 	private SingletonHolder(){}
 	private static class Holder {
-		private static final SingletonHolder instance = new SingletonHolder();
+		private static final SingletonHolder INSTANCE = new SingletonHolder();
 	}
 	public static SingletonHolder getInstance() {
-		return Holder.instance;
+		return Holder.INSTANCE;
 	}
 }
